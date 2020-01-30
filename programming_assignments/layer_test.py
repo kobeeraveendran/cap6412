450d335c308e29ebe799a966b9f11df43ff67409
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-01-30 11:18:52 -0500

    added alternative custom method for loading train and test sets

--- programming_assignments/layer_test.py
+++ programming_assignments/layer_test.py
@@ -5,6 +5,14 @@ from keras.layers import Input, Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, ZeroPadding2D
 from keras.models import Sequential
 from keras.optimizers import Adam
+from keras.metrics import accuracy
+
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import GlobalAveragePooling2D
+from keras import backend as K
+
 from utils import load_data
 
 from tensorflow.compat.v1 import ConfigProto, InteractiveSession
@@ -16,7 +24,7 @@ config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config = config)
 
-def train_test(model, train, test_set, test_labels, label_map):
+def train_model(model, train):
 
     model.compile(optimizer = Adam(), loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
@@ -26,29 +34,26 @@ def train_test(model, train, test_set, test_labels, label_map):
 
     print('\n\nTotal training time taken (mins:sec): {0}:{1:.2f}\n\n'.format(int(training_time // 60), training_time % 60))
 
+    return model
+
+def predict(model, test_set, test_labels):
+
     preds = model.predict(test_set, verbose = 1)
 
-    print('Predicted class indices:\n')
+    print('Predicted Class Indices: \n')
     pred_class_indices = np.argmax(preds, axis = 1)
     print(pred_class_indices)
 
-    print('Num predictions: ', len(pred_class_indices))
-
-    test_acc = 0
     num_test = len(pred_class_indices)
 
-    for i in range(num_test):
-        if label_map[pred_class_indices[i]] == test_labels[i]:
-            test_acc += 1
+    print('Num predictions: ', num_test)
 
-    print('\nTest set accuracy: {0:.2f}'.format(test_acc / num_test * 100))
+    test_acc = accuracy(test_labels, pred_class_indices)
 
-if __name__ == "__main__":
+    print('\nTest set accuracy: {}'.format(np.sum(test_acc) / num_test))
 
-    # load train and test set
-    train, test, test_labels = load_data()
 
-    label_map = {v:k for k, v in train.class_indices.items()}
+def model_3conv():
 
     model = Sequential()
     model.add(Conv2D(64, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
@@ -67,20 +72,115 @@ if __name__ == "__main__":
     model.add(Dense(1024, activation = 'relu'))
     model.add(Dense(200, activation = 'softmax'))
 
-    train_test(model, train, test, test_labels, label_map)
+    return model
 
+def model_6conv():
+
+    model = Sequential()
+    model.add(Conv2D(64, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+    model.add(MaxPooling2D(strides = (2, 2), padding = 'same'))
+    model.add(ZeroPadding2D())
+
+    model.add(Conv2D(128, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(strides = (2, 2), padding = 'same'))
+    model.add(ZeroPadding2D())
+
+    model.add(Conv2D(256, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(strides = (2, 2), padding = 'same'))
+    model.add(ZeroPadding2D())
+
+    model.add(Conv2D(256, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(strides = (2, 2), padding = 'same'))
+    model.add(ZeroPadding2D())
+
+    model.add(Conv2D(128, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(strides = (2, 2), padding = 'same'))
+    model.add(ZeroPadding2D())
+
+    model.add(Conv2D(64, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(strides = (2, 2), padding = 'same'))
+    model.add(ZeroPadding2D())
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation = 'relu'))
+    model.add(Dense(200, activation = 'softmax'))
+
+    return model
+
+if __name__ == "__main__":
+
+    # load train and test set
+    train, test, test_labels = load_data()
+
+    #label_map = {v:k for k, v in train.class_indices.items()}
+
+    # 3 convolutional blocks
     '''
-    model.compile(optimizer = Adam(), loss = 'categorical_crossentropy', metrics = ['accuracy'])
-
-    start = time.time()
-    model.fit_generator(train, epochs = 20, verbose = 1)
-    training_time = time.time() - start
-
-    print('\n\nTotal training time taken (mins:sec): {0}:{1:.2f}\n\n'.format(int(training_time//60), training_time % 60))
-
-    preds = model.predict(test, verbose = 1)
-
-    pred_class_indices = np.argmax(preds, axis = 1)
-
-    print(pred_class_indices)
+    conv3 = model_3conv()
+    trained_model = train_model(conv3, train)
+    predict(trained_model, test, test_labels)
     '''
+
+    # 6 convolutional blocks
+    '''
+    conv6 = model_6conv()
+    trained_model = train_model(conv6, train)
+    predict(trained_model, test, test_labels)
+    '''
+
+    
+    # InceptionV3 pretrained model fine-tuning
+
+    
+    # create the base pre-trained model
+    base_model = InceptionV3(weights='imagenet', include_top=False, input_tensor = Input(shape = (299, 299, 3)))
+
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    # and a logistic layer -- let's say we have 200 classes
+    predictions = Dense(200, activation='softmax')(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics = ['accuracy'])
+
+    # train the model on the new data for a few epochs
+    model.fit_generator(train, epochs = 5, verbose = 1)
+
+    # at this point, the top layers are well trained and we can start fine-tuning
+    # convolutional layers from inception V3. We will freeze the bottom N layers
+    # and train the remaining top layers.
+
+    # let's visualize layer names and layer indices to see how many layers
+    # we should freeze:
+    for i, layer in enumerate(base_model.layers):
+        print(i, layer.name)
+
+    # we chose to train the top 2 inception blocks, i.e. we will freeze
+    # the first 249 layers and unfreeze the rest:
+    for layer in model.layers[:249]:
+        layer.trainable = False
+    for layer in model.layers[249:]:
+        layer.trainable = True
+
+    # we need to recompile the model for these modifications to take effect
+    # we use SGD with a low learning rate
+    from keras.optimizers import SGD
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics = ['accuracy'])
+
+    # we train our model again (this time fine-tuning the top 2 inception blocks
+    # alongside the top Dense layers
+    model.fit_generator(train, epochs = 10, verbose = 1)
+
+    predict(model, test, test_labels)
+    
--- programming_assignments/utils.py
+++ programming_assignments/utils.py
@@ -5,6 +5,8 @@ from keras.preprocessing.image import ImageDataGenerator, load_img
 import numpy as np
 import os
 import cv2
+import re
+from PIL import Image
 
 #from tf.compat.v1 import ConfigProto
 
@@ -13,17 +15,31 @@ def load_data():
     datagen = ImageDataGenerator(rescale = 1. / 255)
 
     # load training data with associated classes from directory structure
-    train_gen = datagen.flow_from_directory('tiny-imagenet-200/train', target_size = (64, 64), batch_size = 128, class_mode = 'categorical')
+
+    # for inception
+    train_gen = datagen.flow_from_directory('tiny-imagenet-200/train', target_size = (299, 299), batch_size = 128, class_mode = 'categorical')
+    
+    # custom
+    #train_gen = datagen.flow_from_directory('tiny-imagenet-200/train', target_size = (64, 64), batch_size = 128, class_mode = 'categorical')
     
     # load test images
     test_dir = os.path.join(os.getcwd(), 'tiny-imagenet-200/val/images/')
     test_set = []
+
+    i = 0
     
-    for sample in os.listdir(test_dir):
-        img = cv2.imread(test_dir + sample)
+    im_dir = sorted(os.listdir(test_dir), key = lambda f: int(re.sub(r'\D', '', f)))
+
+    for sample in im_dir:
+        
+        # inception
+        img = cv2.resize(cv2.imread(test_dir + sample), (299, 299))
+
+        # custom
+        #img = cv2.imread(test_dir + sample)
         test_set.append(np.asarray(img))
 
-    print(test_set[0])
+    #print(test_set[0])
 
     test_set = np.array(test_set)
 
@@ -32,10 +48,65 @@ def load_data():
 
     for line in file:
         img_label = line.split('\t')[1]
-        test_labels.append(img_label)
+        test_labels.append(train_gen.class_indices[img_label])
 
     file.close()
+    test_labels = np.array(test_labels)
 
     #test_gen = datagen.flow_from_directory('tiny-imagenet-200/val/images', target_size = (64, 64))
 
     return train_gen, test_set, test_labels
+
+def load_data_limited(num_samples):
+
+    # load training set
+
+    train_x = np.zeros(shape = (200 * num_samples, 3, 64, 64), dtype = 'uint8')
+    train_y = np.zeros(shape = (200 * num_samples), dtype = 'uint8')
+
+    train_dir_path = 'tiny-imagenet-200/train/'
+
+    label_to_class_index = {}
+
+    sample_num = 0
+    for i, subdir in enumerate(os.listdir(train_dir_path)):
+        imgs_path = os.path.join(train_dir_path, subdir, 'images')
+        label_to_class_index[subdir] = i
+
+        for img in os.listdir(imgs_path):
+            sample = np.asarray(Image.open(os.path.join(imgs_path, img)))
+
+            train_x[sample_num] = sample
+            train_y[sample_num] = i
+            sample_num += 1
+
+    # load test set (using val instead)
+
+    test_x = np.zeros(shape = (200 * 50, 3, 64, 64), dtype = 'uint8')
+    test_y = np.zeros(shape = (200 * 50), dtype = 'uint8')
+
+    test_dir_path = 'tiny-imagenet-200/val/images'
+
+    file = open('tiny-imagenet-200/val/val_annotations.txt', 'r')
+
+    test_label_mapping = {}
+    for line in file:
+        sample_label = line.split('\t')
+        file_name = sample_label[0]
+        class_label = sample_label[1]
+        test_label_mapping[file_name] = class_label
+
+    file.close()
+
+    sample_num = 0
+    for i, img in enumerate(os.listdir(test_dir_path)):
+        sample = np.asarray(Image.open(os.path.join(test_dir_path, img)))
+
+        test_x[sample_num] = sample
+        test_y[sample_num] = int(label_to_class_index[test_label_mapping[img]])
+
+
+    return train_x, train_y, test_x, test_y
+
+if __name__ == '__main__':
+    train, test, test_labels = load_data()
