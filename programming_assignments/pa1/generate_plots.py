0e39e2126cb7ee02d35278443b2b99fab77112b2
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-02-03 23:55:52 -0500

    reverted plot generation to be more generic

--- programming_assignments/pa1/generate_plots.py
+++ programming_assignments/pa1/generate_plots.py
@@ -22,13 +22,13 @@ def generate_plot(title, x_label, y_label, x_vals, y1_vals, y2_vals = None, anno
 if __name__ == '__main__':
     print('Enter plot information...')
     title = input('Title: ')
-    #x_label = input('X-axis label: ')
-    #y_label = input('Y-axis label: ')
-    x_vals = [2.13, 6.91, 12.95]
-    y1_vals = [490.99, 367.47, 1002.32]
+    x_label = input('X-axis label: ')
+    y_label = input('Y-axis label: ')
+    #x_vals = [2.13, 6.91, 12.95]
+    #y1_vals = [490.99, 367.47, 1002.32]
     #y2_vals = [1.64, 1.51, 1.44, 1.39, 1.35, 0.45, 0.34, 0.31, 0.29, 0.29, 0.27, 0.26, 0.25, 0.25, 0.24]
-    #x1_vals = [float(x) for x in input('Enter x-values separated by any whitespace: ').split()]
-    #y1_vals = [float(x) for x in input('Enter y-values separated by any whitespace: ').split()]
-    x_label = 'Number of parameters (in millions)'
-    y_label = 'Training time (seconds)'
+    x_vals = [float(x) for x in input('Enter x-values separated by any whitespace: ').split()]
+    y1_vals = [float(x) for x in input('Enter y-values separated by any whitespace: ').split()]
+    #x_label = 'Number of parameters (in millions)'
+    #y_label = 'Training time (seconds)'
     generate_plot(title, x_label, y_label, x_vals, y1_vals)
